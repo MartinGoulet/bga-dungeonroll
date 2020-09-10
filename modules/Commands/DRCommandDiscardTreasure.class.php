c617e1b55985f29336ddc66e130fdcbfa427f405
Martin Goulet <martin.goulet@live.ca>
2020-09-10 16:56:25 -0400

    DRCommandDiscardTreasure : Exclure dragons dice.

--- modules/Commands/DRCommandDiscardTreasure.class.php
+++ modules/Commands/DRCommandDiscardTreasure.class.php
@@ -14,14 +14,14 @@ class DRCommandDiscardTreasure extends DRCommand
 
     public function canExecute()
     {
-        $itemsInPlay = $this->game->components->getActivePlayerItemsByZone(ZONE_PLAY);
+        $itemsInPlay = $this->getItemsInPlay();
         $hero = $this->game->components->getActivePlayerHero();
         return sizeof($itemsInPlay) == $hero->getNumberTreasureTokenToDiscard();
     }
 
     public function execute($sub_command_id)
     {
-        $itemsInPlay = $this->game->components->getActivePlayerItemsByZone(ZONE_PLAY);
+        $itemsInPlay = $this->getItemsInPlay();
         $itemsInPlay = DRItem::setZone($itemsInPlay, ZONE_BOX);
 
         $this->game->manager->updateItems($itemsInPlay);
@@ -30,5 +30,14 @@ class DRCommandDiscardTreasure extends DRCommand
         // Go to the next state
         $previous_state = $this->game->vars->getChooseDieState();
         $this->game->gamestate->nextState($previous_state);
+    }
+
+    public function getItemsInPlay()
+    {
+        $itemsInPlay = $this->game->components->getActivePlayerItemsByZone(ZONE_PLAY);
+        $itemsInPlay = DRUtils::filter($itemsInPlay, function($item) {
+            return !DRDungeonDice::isDragon($item);
+        });
+        return $itemsInPlay;
     }
 }
